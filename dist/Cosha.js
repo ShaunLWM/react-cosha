d9dffd0c8c281005fa04e39b700571a27e9cdbe3
ShaunLWM <shaunlimweimin@gmail.com>
2020-04-19 00:56:36 +0800

    Add dist files

---
+++ dist/Cosha.js
@@ -0,0 +1,13 @@
+function _extends() { _extends = Object.assign || function (target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i]; for (var key in source) { if (Object.prototype.hasOwnProperty.call(source, key)) { target[key] = source[key]; } } } return target; }; return _extends.apply(this, arguments); }
+
+import React from "react";
+import "./Cosha.css";
+export default function Cosha(props) {
+  return /*#__PURE__*/React.createElement("div", {
+    className: "cosha-wrapper"
+  }, /*#__PURE__*/React.createElement("img", _extends({}, props, {
+    className: "cosha"
+  })), /*#__PURE__*/React.createElement("img", _extends({}, props, {
+    className: "cosha-clone"
+  })));
+}
