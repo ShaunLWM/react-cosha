a2428443cfe319761848502f66ed1f7c58f754b6
ShaunLWM <shaunlimweimin@gmail.com>
2020-04-19 11:38:00 +0800

    Dynamic styles for configurations

--- dist/Cosha.js
+++ dist/Cosha.js
@@ -1,13 +1,33 @@
 function _extends() { _extends = Object.assign || function (target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i]; for (var key in source) { if (Object.prototype.hasOwnProperty.call(source, key)) { target[key] = source[key]; } } } return target; }; return _extends.apply(this, arguments); }
 
 import React from "react";
-import "./Cosha.css";
-export default function Cosha(props) {
+export default function Cosha({
+  className = "cosha",
+  blur = "5px",
+  brightness = 1,
+  saturation = 1,
+  x = 0,
+  y = 0
+}) {
+  const wrapper = {
+    position: "relative",
+    display: "inline-flex",
+    alignItems: "center",
+    justifyContent: "center"
+  };
+  const clone = {
+    filter: `blur(${blur}) brightness(${brightness}) saturate(${saturation})`,
+    position: "absolute",
+    width: "100%",
+    zIndex: -1,
+    transform: `translate3d(${x}, ${y}, 0)`
+  };
   return /*#__PURE__*/React.createElement("div", {
-    className: "cosha-wrapper"
+    style: wrapper
   }, /*#__PURE__*/React.createElement("img", _extends({}, props, {
-    className: "cosha"
+    className: className
   })), /*#__PURE__*/React.createElement("img", _extends({}, props, {
-    className: "cosha-clone"
+    className: `${className}-clone`,
+    style: clone
   })));
 }
--- src/Cosha.jsx
+++ src/Cosha.jsx
@@ -1,11 +1,27 @@
 import React from "react";
-import "./Cosha.css";
 
 export default function Cosha(props) {
+	const { className = "cosha", blur = "5px", brightness = 1, saturation = 1, x = 0, y = 0 } = props;
+
+	const wrapper = {
+		position: "relative",
+		display: "inline-flex",
+		alignItems: "center",
+		justifyContent: "center",
+	};
+
+	const clone = {
+		filter: `blur(${blur}) brightness(${brightness}) saturate(${saturation})`,
+		position: "absolute",
+		width: "100%",
+		zIndex: -1,
+		transform: `translate3d(${x}, ${y}, 0)`,
+	};
+
 	return (
-		<div className="cosha-wrapper">
-			<img {...props} className="cosha"></img>
-			<img {...props} className="cosha-clone"></img>
+		<div style={wrapper}>
+			<img {...props} className={className}></img>
+			<img {...props} className={`${className}-clone`} style={clone}></img>
 		</div>
 	);
 }
